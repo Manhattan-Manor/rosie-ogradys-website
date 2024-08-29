b511d1164e4bbf49c64d989baf7264420fd95d4d
Williams García <williams.0199gd@gmail.com>
2024-08-29 15:23:01 -0600

    view menus in html

--- src/classes/RestaurantMenuCategory.ts
+++ src/classes/RestaurantMenuCategory.ts
@@ -2,15 +2,24 @@ import Dish from "./Dish";
 
 class RestaurantMenuCategory {
   name: string;
-  dishes: Dish[];
+  dishes?: Dish[];
+  subCategory?: RestaurantMenuCategory[];
+  price?: number;
+  description?: string
 
   constructor(category?: RestaurantMenuCategory) {
     if (category) {
       this.name = category.name;
       this.dishes = category.dishes;
+      this.subCategory = category.subCategory;
+      this.price = category.price
+      this.description = category.description;
     } else {
       this.name = "";
       this.dishes = [];
+      this.subCategory = [];
+      this.price = 0;
+      this.description = "";
     }
   }
 }

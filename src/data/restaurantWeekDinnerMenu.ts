b22c0db796521a3472c6905d2ce8d5c56fc49d91
Williams García <williams.0199gd@gmail.com>
2025-01-30 19:19:08 -0600

    Lunch and Dinner Restaurant menus added

---
+++ src/data/restaurantWeekDinnerMenu.ts
@@ -0,0 +1,56 @@
+import RestaurantMenu from "../classes/RestaurantMenu";
+
+const NYCRestaurantDinnerMenu: RestaurantMenu = {
+    name: "NYC Restauran Week® 2025",
+    categories: [
+      {
+        name: "Choice of Appetizer",
+        subCategory: [
+          {
+            name: "French Onion Soup",
+          },
+          {
+            name: "Classic Caesar Salad"
+          },
+          {
+            name: "Smoked Salmon Platter",
+            dishes: [{name: "toast points, capers, onion, and horseradish cream"}],
+          }
+        ],
+      },
+      {
+        name: "Choice of Entrée",
+        subCategory: [
+            {
+                name: "Tuna Steak – Grilled or Blackened",
+                dishes: [{name: "pineapple salsa, sautéed spinach & mashed potatoes"}]
+            },
+            {
+                name: "Short Rib Bourguignon",
+                dishes: [{name:"red wine sauce with vegetable risotto"}]
+            },
+            {
+                name:"Chicken Piccata",
+                dishes:[{name: "lemon caper sauce with sautéed spinach & mashed potatoes"}]
+            }
+        ]
+      },
+      {
+        name: "Choice of Dessert",
+        subCategory: [
+            {
+            name: "Crème Brulée",
+            },
+            {
+                name: "NY Cheesecake with Strawberry Coulis"
+            },
+            {
+                name: "Molton Chocolate Cake à la Mode"
+            }
+        ]
+      }
+    ],
+    
+  };
+
+export default NYCRestaurantDinnerMenu;
---
+++ src/data/restaurantWeekLunchMenu.ts
@@ -0,0 +1,48 @@
+import RestaurantMenu from "../classes/RestaurantMenu";
+
+const NYCRestaurantLunchMenu: RestaurantMenu = {
+    name: "NYC Restauran Week® 2025",
+    categories: [
+      {
+        name: "Choice of Appetizer",
+        subCategory: [
+          {
+            name: "Classic Caesar Salad"
+          },
+          {
+            name: "Soup of the Day",
+          },
+          {
+            name: "House Salad"
+          }
+        ],
+      },
+      {
+        name: "Choice of Entrée",
+        subCategory: [
+            {
+                name: "Mediterranean Grilled Salmon",
+                dishes: [{name: "olives, mushrooms, artichoke hearts and tomatoes with broccoli & mashed potatoes"}]
+            },
+            {
+                name: "Chicken Saltimbocca",
+                dishes: [{name:"filled with prosciutto, sage, and mozzarella over linguine"}]
+            }
+        ]
+      },
+      {
+        name: "Choice of Dessert",
+        subCategory: [
+            {
+                name: "NY Cheesecake with Strawberry Coulis",
+            },
+            {
+                name: "Chocolate Raspberry Cake"
+            }
+        ]
+      }
+    ],
+    
+  };
+
+export default NYCRestaurantLunchMenu;
